19bf8662bab3b5524df5143535b73691e5d4e367
daniel <daniel@theheretic.work>
2025-07-24 12:26:01 -0500

    0.9.1

---
+++ server/index.ts
@@ -0,0 +1,166 @@
+import { createDeskThing } from '@deskthing/server';
+import { AppSettings, DESKTHING_EVENTS, SETTING_TYPES } from '@deskthing/types';
+import dotenv from 'dotenv';
+import { existsSync } from "node:fs";
+import { mkdir, stat } from "node:fs/promises";
+import { saveImageReferenceFromURL } from './utils';
+
+dotenv.config();
+
+type ToClientData = {
+  type: 'imageData', payload: string
+}
+
+enum IMAGE_REQUESTS {
+  GET = 'get'
+}
+
+type GenericTransitData = {
+  type: IMAGE_REQUESTS.GET, request: 'image', payload?: string
+}
+
+const DeskThing = createDeskThing<GenericTransitData, ToClientData>()
+let v: any = 0
+
+const sendImage = (imageUrls: string[]) => {
+  const imageUrl: string = imageUrls[Math.floor(Math.random() * imageUrls.length)]
+  console.error(imageUrl)
+  console.error(imageUrls.join(", "))
+
+  DeskThing.send({
+    type: 'imageData', payload: imageUrl
+  });
+}
+
+const ensureFileExists = (imageUrl) => {
+  if (!existsSync(imageUrl)) {
+    console.debug('Creating images directory');
+    mkdir(imageUrl, { recursive: true });
+  }
+}
+
+
+async function isPathDirectory(path: string): Promise<boolean> {
+  try {
+    const stats = await stat(path); // Or use fs.stat with a callback
+    return stats.isDirectory();
+  } catch (error) {
+    // Handle cases where the path doesn't exist or is inaccessible
+    return false;
+  }
+}
+
+
+
+const sendImageToClient = async (imagePath: string) => {
+  try {
+    const imageUrl = process.env.DESKTHING_ENV == 'development' ? imagePath : await saveImageReferenceFromURL(imagePath)
+
+    if (!imageUrl) {
+      console.error('Error saving image reference');
+      return;
+    }
+
+    console.debug('Sending Image ' + imageUrl + ' to client')
+
+    if (typeof imageUrl == "object") {
+      sendImage(imageUrl)
+    } else {
+      DeskThing.send({
+        type: 'imageData', payload: imageUrl
+      });
+    }
+  } catch (error) {
+    console.error('Error reading image file: ' + error);
+  }
+};
+
+const start = async () => {
+  const settings: AppSettings = {
+    image_source: {
+      value: '',
+      id: 'image_source',
+      type: SETTING_TYPES.STRING,
+      label: "Image URL",
+      description: 'Use a file path or a web url that will be used to fetch the image',
+    },
+    image_directory: {
+      value: '',
+      id: 'image_directory',
+      type: SETTING_TYPES.STRING,
+      label: "Image FILE",
+      description: 'Shortcut for selecting an image file from your computer',
+      // fileTypes: [
+      //   {
+      //     name: 'Image Directory',
+      //     extensions: ["*"]
+      //   },
+      //   {
+      //     name: 'Image Files',
+      //     extensions: ["*"]
+      //   }
+      // ]
+    },
+  }
+  DeskThing.initSettings(settings)
+}
+
+const stop = async () => {
+  // Function called when the server is stopped
+}
+
+DeskThing.on(DESKTHING_EVENTS.SETTINGS, async (setting) => {
+  // First check if there is an image file provided
+  if (setting.payload.image_directory.value && setting.payload.image_directory.type == SETTING_TYPES.STRING) {
+    DeskThing.setSettings({
+      image_source: {
+        value: setting.payload.image_directory.value, // sets the value to the image_directory value
+        id: 'image_source',
+        type: SETTING_TYPES.STRING,
+        label: "Image URL",
+        description: 'Use a file path or a web url that will be used to fetch the image',
+      },
+      image_directory: {
+        value: '',
+        id: 'image_directory',
+        type: SETTING_TYPES.STRING,
+        label: "Image FILE",
+        description: 'Shortcut for selecting an image file from your computer',
+      }
+    });
+    await sendImageToClient(setting.payload.image_directory.value);
+  } else if (setting.payload.image_source.value && setting.payload.image_source.type == SETTING_TYPES.STRING) {
+    await sendImageToClient(setting.payload.image_source.value);
+  } else {
+    console.warn('No image source found in settings!');
+  }
+
+
+})
+
+DeskThing.on(IMAGE_REQUESTS.GET, async (data) => {
+  if (data.type == null) {
+    console.warn('No args provided!')
+    return
+  }
+  switch (data.request) {
+    case 'image':
+      const Data = await DeskThing.getSettings()
+      if (Data?.image_source.value && Data.image_source.type == SETTING_TYPES.STRING) {
+        await sendImageToClient(Data?.image_source.value)
+      } else {
+        console.warn('No image source found!')
+      }
+      break
+    default:
+      console.warn(`Unknown request: ${data.request}`)
+      break
+    // Handle other types ?
+  }
+})
+
+// Main Entrypoint of the server
+DeskThing.on(DESKTHING_EVENTS.START, start)
+
+// Main exit point of the server
+DeskThing.on(DESKTHING_EVENTS.STOP, stop)
